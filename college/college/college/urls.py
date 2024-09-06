dd4bb80b15eebc7949d80bcf1be2de32debb38f2
JagrutiPatil151199 <patiljagruti209@gmail.com>
2024-09-06 07:03:21 +0530

    Collage Python Django Project initial commit

---
+++ college/college/college/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for college project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from student.views import StudentListCreate, StudentRetrieveUpdateDestroy
+from teacher.views import TeacherListCreate, TeacherRetrieveUpdateDestroy
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('students/', StudentListCreate.as_view(), name='student-list-create'),
+    path('students/<int:pk>/', StudentRetrieveUpdateDestroy.as_view(), name='student-detail'),
+    path('teachers/', TeacherListCreate.as_view(), name='teacher-list-create'),
+    path('teachers/<int:pk>/', TeacherRetrieveUpdateDestroy.as_view(), name='teacher-detail'),
+
+]
---
+++ college/college/student/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Student
+from .serializers import StudentSerializer
+
+# CRUD for Student
+class StudentListCreate(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
